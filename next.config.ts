2d064128a9955c39f1e0791c2a09db8dcd42558e
RIANSETIYAWANSKOM <riansetiyawanskom@gmail.com>
2026-01-20 08:21:31 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 
-const LOADER = path.resolve(
-  __dirname,
-  "src/visual-edits/component-tagger-loader.js"
-);
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -17,13 +12,6 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [LOADER],
-      },
-    },
   },
 };
 
