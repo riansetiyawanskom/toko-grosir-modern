1a73575b822c43ea23529fcd31f3f0394f7edb28
RIANSETIYAWANSKOM <riansetiyawanskom@gmail.com>
2026-01-20 08:18:11 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,30 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 
-const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+const LOADER = path.resolve(
+  __dirname,
+  "src/visual-edits/component-tagger-loader.js"
+);
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "**",
       },
     ],
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   turbopack: {
     rules: {
       "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  }
+        loaders: [LOADER],
+      },
+    },
+  },
 };
 
 export default nextConfig;
